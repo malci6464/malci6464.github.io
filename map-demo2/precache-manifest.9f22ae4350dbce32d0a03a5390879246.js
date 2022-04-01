76e1c9bb9d580fdd8f52eabb83fe8a5672565140
Malcolm Isaacs <mis@telenormaritime.com>
2022-04-01 22:05:17 +0200

    react demo gh pages test dploy2

---
+++ map-demo2/precache-manifest.9f22ae4350dbce32d0a03a5390879246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3f615d5c4ae27392cac69d981dacd1",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "ba4b4343ee883fa6ff2a",
+    "url": "/demo-app/static/css/main.f067808c.chunk.css"
+  },
+  {
+    "revision": "64e180015c1a0f26ff56",
+    "url": "/demo-app/static/js/2.fe75a9fa.chunk.js"
+  },
+  {
+    "revision": "ba4b4343ee883fa6ff2a",
+    "url": "/demo-app/static/js/main.ec5776e9.chunk.js"
+  },
+  {
+    "revision": "db7ffb4ffc3f496ab832",
+    "url": "/demo-app/static/js/runtime~main.8553000d.js"
+  }
+]);
